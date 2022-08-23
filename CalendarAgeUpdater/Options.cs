5273f604f91afb4e5c63b3983f3ef0392da8ed6f
Tomas Svoboda <tomas@svobodovi.xyz>
2022-08-23 20:52:32 +0200

    CommandLine parser usage

---
+++ CalendarAgeUpdater/Options.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+
+namespace tomxyz.googleapi
+{
+    public class Options
+    {
+        [Option('r', "regex", Required = true, HelpText = "Regex matching events in calendar with group matching year and groups that you can address in target pattern. Sample: (.*) - birthday \\((.*)\\)")]
+        public string Regex { get; set; }
+
+        [Option('g', "group", Required = true, HelpText = "Index of group with birth year (start from 1)")]
+        public int Group { get; set; }
+
+        [Option('p', "pattern", Required = true, HelpText = "Pattern of target event name where '$number' is marking groups from regex_to_find and $Y stands for age.\r\n\tCharacter + is delimeter. Sample: $1+$Y+($2)")]
+        public string Patttern { get; set; }
+
+        [Option('y', "year", Required = false, HelpText = $"year that will be used for event listing. Current year is default)")]
+        public int? Year { get; set; }
+    }
+}
+
--- CalendarAgeUpdater/Program.cs
+++ CalendarAgeUpdater/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using CommandLine;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace tomxyz.googleapi
@@ -6,20 +7,12 @@ namespace tomxyz.googleapi
     class CalendarAgeUpdater
     {
         /// <summary>
-        /// Print help
+        /// Print usage help
         /// </summary>
-        private static void PrintHelp()
+        private static void PrintUsage()
         {
-            var now = DateTime.Now;
-            Console.WriteLine("Run as: \r\nCalendarAgeUpdater.exe  group_with_age target_pattern\r\n [year]");
-            Console.WriteLine("regex_to_find\r\n\tregex matching events in calendar with group matching year and groups that you can address in target pattern. Sample: (.*) - birthday \\((.*)\\)\r\n");
-            Console.WriteLine("group_with_year\r\n\tindex of group with birth year\r\n");
-            Console.WriteLine("target_pattern\r\n\tpattern of target event name where '$number' is marking groups from regex_to_find and $Y stands for age.\r\n\tCharacter + is delimeter. Sample: $1+$Y+($2)");
-            Console.WriteLine($"year\r\n\tyear that will be used for event listing. Current year is default ({now.Year})");
-
-            Console.WriteLine();
             Console.WriteLine($"Consider that you have event 'John Smith - birthday (2002)' in your calendar and want to add proper age for year 2022.");
-            Console.WriteLine($"You can run:\r\nCalendarAgeUpdater.exe \"(.*) - birthday \\((.*)\\)\" 2 \"$1+ $Y+ ($2)\"");
+            Console.WriteLine($"You can run:\r\nCalendarAgeUpdater.exe -r \"(.*) - birthday \\((.*)\\)\" -g 2 -p \"$1+ $Y+ ($2)\"");
             Console.WriteLine($"Event is updated to: 'John Smith 20 (2022)'");
         }
 
@@ -76,109 +69,103 @@ namespace tomxyz.googleapi
             }
         }
 
-        public static async Task<int> Main(string[] args)
+        public static async Task StartProcessingAsync(string regexFind, int yearGroup, int? eventsYear, string targetPattern)
+        {
+            var rFind = new Regex(regexFind, RegexOptions.IgnoreCase);
+            if (!eventsYear.HasValue)
+                eventsYear = DateTime.Now.Year;
+
+            // set utf-8 encoding for console output
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var calendarApi = new CalendarApi();
+            Console.WriteLine("Sign in to your Google account and consent permissions for the application");
+
+            // authenticate
+            await calendarApi.AutenticateAsync("auth.json");
+
+            // list calendars
+            int i = 0;
+            var calendars = (await calendarApi.ListCalendarsAsync()).ToDictionary(x => ++i);
+
+            Console.WriteLine("Listed calendars (select target): \r\n");
+            foreach (var (n, c) in calendars)
+                Console.WriteLine($"{n}: {c.Name}");
+
+            Console.WriteLine();
+            var read = Console.ReadLine();
+            if (string.IsNullOrEmpty(read))
+            {
+                Console.WriteLine("No calendar selected. Exiting.");
+            }
+
+            var index = int.Parse(read);
+            var calendar = calendars[index];
+            Console.WriteLine($"Selected calendar: {index} - {calendar.Name}");
+            Console.WriteLine();
+
+            var events = await calendarApi.GetAllEventsAsync(calendar.Id, eventsYear.Value);
+            var filtered = events
+                .Select(x => (rFind.Match(x.Description), x))
+                .Where(x => x.Item1.Success)
+                .Select(x => (x.Item2, CalculateNewEventSummary(x.Item1, targetPattern, yearGroup, eventsYear.Value)));
+
+            while (true)
+            {
+                Console.WriteLine($"Press: \r\n\ta\tlist all events\r\n\tf\tlist filtered events\r\n\tr\tpairs of results\r\n\ts\tstart renaming\r\n\tother\texit");
+                read = Console.ReadLine();
+
+                if (read == "a")
+                {
+                    foreach (var e in events)
+                        Console.WriteLine($"{e.Date}: {e.Description}");
+                }
+                else if (read == "f")
+                {
+                    foreach (var pair in filtered)
+                        Console.WriteLine($"{pair.Item1.Date}: {pair.Item1.Description}");
+                }
+                else if (read == "r")
+                {
+                    foreach (var pair in filtered)
+                        Console.WriteLine($"{pair.Item1.Date}: {pair.Item1.Description} -> {pair.Item2}");
+                }
+                else if (read == "s")
+                {
+                    foreach (var pair in filtered)
+                    {
+                        if (!string.IsNullOrEmpty(pair.Item2))
+                            await calendarApi.UpdateEventSummaryAsync(calendar.Id, pair.Item1.Id, pair.Item2);
+                    }
+
+                    Console.WriteLine($"Events updated. New events in calendar: ");
+                    events = await calendarApi.GetAllEventsAsync(calendar.Id, eventsYear.Value);
+                    foreach (var e in events)
+                        Console.WriteLine($"{e.Date}: {e.Description}");
+
+                    break;
+                }
+                else
+                    break;
+            }
+        }
+
+        public static int Main(string[] args)
         {
             try
             {
-                string regexFind = string.Empty;
-                Regex rFind;
-                int yearGroup = 0;
-                int eventsYear = DateTime.Now.Year;
+                Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
 
-                string targetPattern = string.Empty;
+                       StartProcessingAsync(o.Regex, o.Group, o.Year, o.Patttern)
+                       .GetAwaiter().GetResult();
+                   })
+                   .WithNotParsed<Options>(async =>
+                   {
+                       PrintUsage();
+                   });
 
-                // set utf-8 encoding for console output
-                Console.OutputEncoding = Encoding.UTF8;
-
-                if (args.Length >= 3)
-                {
-                    regexFind = args[0];
-                    rFind = new Regex(regexFind, RegexOptions.IgnoreCase);
-                    yearGroup = int.Parse(args[1]);
-                    targetPattern = args[2];
-
-                    if (args.Length > 3)
-                        eventsYear = int.Parse(args[3]);
-
-                    PrintConfiguration(regexFind, yearGroup, targetPattern, eventsYear);
-                }
-                else
-                {
-                    PrintHelp();
-                    return 0;
-                }
-
-                var calendarApi = new CalendarApi();
-                Console.WriteLine("Sign in to your Google account and consent permissions for the application");
-
-                // authenticate
-                await calendarApi.AutenticateAsync("auth.json");
-
-                // list calendars
-                int i = 0;
-                var calendars = (await calendarApi.ListCalendarsAsync()).ToDictionary(x => ++i);
-
-                Console.WriteLine("Listed calendars (select target): \r\n");
-                foreach (var (n, c) in calendars)
-                    Console.WriteLine($"{n}: {c.Name}");
-
-                Console.WriteLine();
-                var read = Console.ReadLine();
-                if (string.IsNullOrEmpty(read))
-                {
-                    Console.WriteLine("No calendar selected. Exiting.");
-                    return 0;
-                }
-
-                var index = int.Parse(read);
-                var calendar = calendars[index];
-                Console.WriteLine($"Selected calendar: {index} - {calendar.Name}");
-                Console.WriteLine();
-
-                var events = await calendarApi.GetAllEventsAsync(calendar.Id, eventsYear);
-                var filtered = events
-                    .Select(x => (rFind.Match(x.Description), x))
-                    .Where(x => x.Item1.Success)
-                    .Select(x => (x.Item2, CalculateNewEventSummary(x.Item1, targetPattern, yearGroup, eventsYear)));
-
-                while (true)
-                {
-                    Console.WriteLine($"Press: \r\n\ta\tlist all events\r\n\tf\tlist filtered events\r\n\tr\tpairs of results\r\n\ts\tstart renaming\r\n\tother\texit");
-                    read = Console.ReadLine();
-
-                    if (read == "a")
-                    {
-                        foreach (var e in events)
-                            Console.WriteLine($"{e.Date}: {e.Description}");
-                    }
-                    else if (read == "f")
-                    {
-                        foreach (var pair in filtered)
-                            Console.WriteLine($"{pair.Item1.Date}: {pair.Item1.Description}");
-                    }
-                    else if (read == "r")
-                    {
-                        foreach (var pair in filtered)
-                            Console.WriteLine($"{pair.Item1.Date}: {pair.Item1.Description} -> {pair.Item2}");
-                    }
-                    else if (read == "s")
-                    {
-                        foreach (var pair in filtered)
-                        {
-                            if (!string.IsNullOrEmpty(pair.Item2))
-                                await calendarApi.UpdateEventSummaryAsync(calendar.Id, pair.Item1.Id, pair.Item2);
-                        }
-
-                        Console.WriteLine($"Events updated. New events in calendar: ");
-                        events = await calendarApi.GetAllEventsAsync(calendar.Id, eventsYear);
-                        foreach (var e in events)
-                            Console.WriteLine($"{e.Date}: {e.Description}");
-
-                        break;
-                    }
-                    else
-                        break;
-                }
 
                 return 0;
             }
